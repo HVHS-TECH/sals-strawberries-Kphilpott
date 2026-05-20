6866c1bd2b3ef8f6abd2404f865f83adb85ff196
Kphilpott <23424kp@hvhs.school.nz>
2026-05-21 11:03:31 +1200

    1.0 "Save"

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 
 console.log("Running Sal's Strawberries")
 
+
+
 function writeForm(){
     // Get the form data
     console.log("Hello World");
+    console.log(GLOBAL_user);
     const UserName = document.getElementById("name").value;
     console.log(UserName);
     const favoriteFruit = document.getElementById("favoriteFruit").value;
@@ -14,18 +17,22 @@ function writeForm(){
 
 
 
-    firebase.database().ref('/users/' + UserName).set(
+    firebase.database().ref('/users/' + GLOBAL_user.uid).set(
       {
+        Username: UserName,
         FavoriteFruit: favoriteFruit,
         QuantityFruit: quantityFruit,
+        Email: GLOBAL_user.email,
       }
     )
+
+    document.getElementById("ThankYou").innerHTML = "Thank You!";
 }
 
 
 
 function fb_write() {
-    console.log("Hello World");
+    console.log("Writing Online.");
 
 
    writeForm()
@@ -36,6 +43,8 @@ function fb_write() {
 
 
 var GLOBAL_user;
+var authenticationListener;
+
 
 function fb_popuplogin() {
   var provider = new firebase.auth.GoogleAuthProvider();
@@ -48,17 +57,27 @@ function fb_popuplogin() {
 
 
 function fb_login() {
-  firebase.auth().onAuthStateChanged(fb_HandleLogin);
+  authenticationListener = firebase.auth().onAuthStateChanged(fb_HandleLogin);
+  document.getElementById("Logout").style.display = "block";
+  document.getElementById("Login").style.display = "none";
 }
 
 function fb_HandleLogin(_user) {
   if (_user) {
     console.log("User is logged in.");
+    console.log(_user);
     GLOBAL_user = _user; //save the user details as global value
-
+    document.getElementById("submit").style.display = "block";
 
   } else {
     console.log("User is NOT logged in, starting the popup process.");
     fb_popuplogin();
   }
+}
+
+function fb_logout() {
+  authenticationListener();
+  firebase.auth().signOut();
+  document.getElementById("Logout").style.display = "none";
+  document.getElementById("Login").style.display = "block";
 }
