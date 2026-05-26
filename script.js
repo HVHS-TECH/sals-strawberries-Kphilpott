2323e5111ac0306c572b79d41383a830b87d55b4
Kphilpott <23424kp@hvhs.school.nz>
2026-05-26 14:50:11 +1200

    1.2.5 changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 console.log("Europa Starships Online, loading order form...")
-console.log("Europa Starships System Version 1.2.4 'Logo' Online.")
+console.log("Europa Starships System Version 1.2.5 'Logo' Online.")
 
 
 function writeForm(){
